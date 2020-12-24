3f869110048374d0a7b368d2a4c335cfefa05d0d
Amita <panwar.amita@gmail.com>
2020-12-24 11:11:34 -0500

    updated the script to add the event

--- assets/script/planner.js
+++ assets/script/planner.js
@@ -1,48 +1,41 @@
-$(document).ready(function()
-{
-    var currentDay=moment().format('dddd MMMM Do');
-    //alert(currentDay);
-    $("#currentDay").text(currentDay);
-    
-    $(".saveBtn").click(function(){
-        var text=$(this).siblings(".schedule").val();
-        var parent=$(this).parent().attr("id");
-      //  console.log(parent);
-        localStorage.setItem(parent,text);   
-      //  console.log(text);
-      alert("The day activity is saved");
+/*function after the page load */
+$(document).ready(function () {
+  /**/
+  var currentDay = moment().format('dddd MMMM Do');
+  $("#currentDay").text(currentDay);
+
+  $(".saveBtn").click(function () {
+    var text = $(this).siblings(".schedule").val();
+    var parent = $(this).parent().attr("id");
+    localStorage.setItem(parent, text);
+    alert("The day activity is saved");
+  });
+  if (localStorage.length > 0) {
+    $(".time-block").each(function () {
+      var id = $(this).attr("id");
+      var text = localStorage.getItem(id);
+      if (text != null) {
+        $(this).children("textArea").val(text);
+        console.log("localstoragevalue", text);
+      }
     });
-    if(localStorage.length>0)
-    {
-        $(".time-block").each(function( ) {
-            var id=$(this).attr("id");
-            var text=localStorage.getItem(id);
-            if(text!=null)
-            {
-                $(this).children("textArea").val(text);
-                console.log("localstoragevalue",text);
-            }
-        });
-}
-    $(".time-block").each(function( ) {
-        console.log("time",moment().format("hh a"));
-        console.log($(this).attr("id"));
-         if($(this).attr("id")<moment().format("hh"))
-         {
-           
-            $(this).children("textArea").addClass("future");
-         }
-         else if($(this).attr("id")>moment().format("hh"))
-         {
-            var child=$(this).children("textArea");
-            console.log("child",child);
-          //  $(this).children("textArea").attr("readonly", "readonly");
-            $(this).children("textArea").addClass("past");
-         }
-         else if($(this).attr("id")===moment().format("hh"))
-         {
-           
-            $(this).children("textArea").addClass("present");
-         }
-      });
+  }
+  $(".time-block").each(function () {
+    console.log("time", moment().hours());
+    console.log($(this).attr("id"));
+    if ($(this).attr("id") > moment().hours()) {
+
+      $(this).children("textArea").addClass("future");
+    }
+    else if ($(this).attr("id") < moment().hours()) {
+      var child = $(this).children("textArea");
+      console.log("child", child);
+      $(this).children("textArea").attr("readonly", "readonly");
+      $(this).children("textArea").addClass("past");
+    }
+    else if ($(this).attr("id") == moment().hours()) {
+      //alert("hi");
+      $(this).children("textArea").addClass("present");
+    }
+  });
 });
